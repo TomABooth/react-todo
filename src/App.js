9cbc88b9fd3cedacfed8d6845e55c5e3066114b7
Tom Booth <tombeezle@gmail.com>
2023-01-30 14:49:36 -0800

    hardcoded and set up files for auth and todos.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header.js';
+import { Route, Switch } from 'react-router-dom';
+import Auth from './Components/Auth/Auth.js';
+import Todos from './Components/Todos/Todos.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/todos" component={Todos} />
+      </Switch>
     </div>
   );
 }
---
+++ src/Components/Todos/Todos.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Todos.css';
+
+export default function Todos() {
+  return (
+    <div className="todo-main">
+      <div className="input-container">
+        <div className="todo-input">
+          <input type="text"></input>
+          <button>Add Todo</button>
+        </div>
+      </div>
+      <div className="todo-container">
+        <ul>
+          <li>hard code</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
